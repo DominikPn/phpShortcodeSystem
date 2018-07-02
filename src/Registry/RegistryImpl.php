153c184182bdb4e5e04c64c9a99e7bd7e0d56e3d
Dominik Pondo <dominikpondo@gmail.com>
2018-07-02 20:43:22 +0200

    Create registry implementation

---
+++ src/Registry/RegistryImpl.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: DesktopHome
+ * Date: 2018-07-02
+ * Time: 20:39
+ */
+
+namespace ShortcodeSystem\Registry;
+
+
+use ShortcodeSystem\Observer\Observer;
+
+class RegistryImpl implements Registry
+{
+    protected $observers = [];
+
+    public function register(Observer $observer, string $ID)
+    {
+        $this->observers[$ID] = $observer;
+    }
+
+    public function findObserverByShortcodeID(string $ID): Observer
+    {
+        if( isset($this->observers[$ID]) ) return $this->observers[$ID];
+    }
+
+}
